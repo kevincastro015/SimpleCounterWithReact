64cd8aa96105842d67ac16e7b628c686438da5b4
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-01 00:49:06 +0000

    andCounting

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [counter, setCounter] = useState(0);
+	const [counter2, setCounter2] = useState(0);
+	if (counter === 10) {
+		setCounter(0);
+	}
+	if (counter2 === 10) {
+		setCounter2(0);
+    }
+	useEffect(() => {
+		setInterval(() => {
+			setCounter(counter => counter + 1);
+		}, 100);
+    useEffect(() => {
+		setInterval(() => {
+			setCounter(counter2 => counter2 + 1);
+		}, 10);
+	}, []);
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>And counting!</h1>
+			<p>{counter}</p>
+			<p>{counter2}</p>
+			{/* {<button onClick={() => setCounter(counter + 1)}>Increment</button>} */}
 		</div>
 	);
 }
+)}
